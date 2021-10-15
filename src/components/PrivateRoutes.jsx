3d18212e826862eefd4b99cb7eb68d2f8a7adc86
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-10-15 13:06:26 +0530

    refactor: removed commented code

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,21 +1,3 @@
-// import { useSelector } from "react-redux";
-// import { Route, Navigate } from "react-router-dom";
-// import { store } from "../app/store";
-
-// const PrivateRoute = ({ path, ...props }) => {
-//     const { isUserLoggedIn } = useSelector((state) => state.auth);
-
-//     console.log("hi")
-
-//     return isUserLoggedIn ? (
-//         <Route path={path} {...props} />
-//     ) : (
-//         <Navigate state={{ from: path }} to="/login" />
-//     );
-// };
-
-// export default PrivateRoute;
-
 import { useSelector } from "react-redux";
 import { Route, Navigate } from "react-router-dom";
 
@@ -23,30 +5,11 @@ const PrivateRoutes = ({ path, ...props }) => {
     const { isUserLoggedIn } = useSelector((state) => state.auth);
 
     console.log("user", { isUserLoggedIn });
-<<<<<<< HEAD
     return isUserLoggedIn ? (
         <Route to={path} {...props} />
     ) : (
         <Navigate state={{ from: path }} to="/login" />
-=======
-    return (
-        <>
-            isUserLoggedIn ? (
-            <Route path="/login" {...props} />
-            ) : (
-            <Navigate state={{ from: path }} to="/login" />)
-        </>
->>>>>>> b44f162720eed33affa52b562eb76d7ab0ed612b
     );
 };
 
 export default PrivateRoutes;
-<<<<<<< HEAD
-=======
-
-// isUserLoggedIn ? (
-//     <Route path="/login" {...props} />
-// ) : (
-//     <Navigate state={{ from: path }} to="/login" />
-// );
->>>>>>> b44f162720eed33affa52b562eb76d7ab0ed612b
