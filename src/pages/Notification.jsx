59eb3ef6c3d1a396c61212427f2a27a4a2f8f9de
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-11-23 22:16:38 +0530

    style: styled notification card

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,13 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import NotificationCard from "../components/NotificationCard";
 import { getNotification } from "../features/notification/request";
 
 const Notification = () => {
     const dispatch = useDispatch();
+    const { notifications, notificationStatus } = useSelector(
+        (state) => state.notification
+    );
     useEffect(() => {
         dispatch(getNotification());
     }, []);
-    return <div>Notification</div>;
+    return (
+        <div>
+            {notificationStatus === "pending" ? (
+                <p>Loading</p>
+            ) : (
+                <div className="notification-container">
+                    {notifications.map((notification) => (
+                        <NotificationCard {...notification} />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Notification;
